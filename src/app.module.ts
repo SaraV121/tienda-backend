2c136d98dd55f91c4aa7fd34f6036374a31b3cee
CamSaraAV <camiltav7@gmail.com>
2026-06-05 13:01:50 -0400

    Configuracion para deploy en Render

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,38 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import * as dotenv from 'dotenv';
+
 import { ClienteModule } from './cliente/cliente.module';
 import { CategoriaModule } from './categoria/categoria.module';
 import { ProductoModule } from './producto/producto.module';
 import { OrdenModule } from './orden/orden.module';
 import { OrdenProductoModule } from './orden-producto/orden-producto.module';
 
+dotenv.config();
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'Zanahorias21',
-      database: 'tienda_db',
+
+      host: process.env.DB_HOST,
+
+      port: parseInt(
+        process.env.DB_PORT || '5432',
+      ),
+
+      username:
+        process.env.DB_USERNAME,
+
+      password:
+        process.env.DB_PASSWORD,
+
+      database:
+        process.env.DB_NAME,
+
       autoLoadEntities: true,
+
       synchronize: true,
     }),
 
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,54 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+} from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+  );
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe(),
+  );
 
-  const config = new DocumentBuilder()
-    .setTitle('Tienda Online')
-    .setDescription('Documentación de la API')
-    .setVersion('1.0')
-    .build();
+  const config =
+    new DocumentBuilder()
+      .setTitle('Tienda Online')
+      .setDescription(
+        'Documentación de la API',
+      )
+      .setVersion('1.0')
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document =
+    SwaggerModule.createDocument(
+      app,
+      config,
+    );
 
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup(
+    'api',
+    app,
+    document,
+  );
 
-  await app.listen(3000);
+  const port =
+    process.env.PORT || 3000;
 
-  console.log(`Servidor corriendo en:
-  http://localhost:3000`);
+  await app.listen(
+    port,
+    '0.0.0.0',
+  );
 
-  console.log(`Swagger:
-  http://localhost:3000/api`);
+  console.log(
+    `Servidor corriendo en puerto ${port}`,
+  );
 }
 
 bootstrap();
