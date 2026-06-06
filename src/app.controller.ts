3f268fcb84a13ebe7b38d4ccebd645a628c52646
CamSaraAV <camiltav7@gmail.com>
2026-06-06 00:57:13 -0400

    Ocultar AppController de Swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
 
-@Controller()
 @ApiExcludeController()
+@Controller()
 export class AppController {
   @Get()
   home() {
